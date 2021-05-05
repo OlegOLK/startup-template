a8ab9ebcde892489e45b5f0ef6b101b68069ec79
Oleg Krotenko <olk@stylelabs.com>
2021-05-05 16:51:06 +0300

    ccc

--- src/Web/Startup.Web/Startup.cs
+++ src/Web/Startup.Web/Startup.cs
@@ -28,7 +28,7 @@ namespace Startup.Web
         {
             services.AddHttpClient();
             services.AddHealthChecks();
-            services.AddSystemMetrics();
+            // services.AddSystemMetrics();
             services.AddDataProtection(s =>
             {
                 s.ApplicationDiscriminator = "Startup.Web";
