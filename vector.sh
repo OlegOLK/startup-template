4b94de61bd5c1ef996e2ce47f84f59e49603ce48
Oleg Krotenko <olk@stylelabs.com>
2021-05-08 00:53:43 +0300

    clean up

--- vector.sh
+++ vector.sh
@@ -7,11 +7,6 @@ sed -i -e "s/\(address=\).*/\1\"0.0.0.0:$PORT\"/" \
 sed -i -e "s/\(auth.password=\).*/\1\"$PROM_PASSWORD\"/" \
 -e "s/\(auth.user=\).*/\1\"$PROM_USER\"/" /etc/vector/vector.prom.toml
 
-
-cat /etc/vector/vector.prom.toml
-
-vector -c /etc/vector/vector.prom.toml
-
-#-c /etc/vector/vector.loki.toml
+vector -c /etc/vector/vector.prom.toml -c /etc/vector/vector.loki.toml
 
 
